f6ab1f8b8a5e02166a804fa2c66b75292d045189
John Orchard <john@work.com>
2011-05-27 12:24:15 +0100

    Added ui module generator.

--- main_program/client/gui_interface_designs/new_ticket_dialog_generated.py
+++ main_program/client/gui_interface_designs/new_ticket_dialog_generated.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'new_ticket_dialog_design.ui'
 #
-# Created: Thu May 26 13:51:39 2011
+# Created: Fri May 27 12:17:03 2011
 #      by: PyQt4 UI code generator 4.8.4
 #
 # WARNING! All changes made in this file will be lost!
--- main_program/client/gui_interface_designs/settings_dialog_generated.py
+++ main_program/client/gui_interface_designs/settings_dialog_generated.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'settings_dialog_design.ui'
 #
-# Created: Wed May 18 11:56:51 2011
+# Created: Fri May 27 12:17:04 2011
 #      by: PyQt4 UI code generator 4.8.4
 #
 # WARNING! All changes made in this file will be lost!
